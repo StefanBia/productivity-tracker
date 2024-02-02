7c0df0807d4d258a66df918ee6d94f40fba26267
Stefan Bia <stefanbia0108@gmail.com>
2024-02-02 20:14:08 +0200

    Create plugin body

--- Productivity-Tracker/src/main/java/com/productivity/productivitytracker/App.java
+++ Productivity-Tracker/src/main/java/com/productivity/productivitytracker/App.java
@@ -1,7 +1,15 @@
 package com.productivity.productivitytracker;
 
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
-public class App {
+public class App extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
 
+        JOptionPane.showMessageDialog(null, "info", "Alert", JOptionPane.INFORMATION_MESSAGE);
+
+    }
 }
