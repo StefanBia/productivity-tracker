7da2fc183ed0ca4970e031885dac9e6aa02e74b2
Stefan Bia <stefanbia0108@gmail.com>
2024-02-03 03:15:53 +0200

    Create line numberer function

--- Productivity-Tracker/src/main/java/com/productivity/productivitytracker/App.java
+++ Productivity-Tracker/src/main/java/com/productivity/productivitytracker/App.java
@@ -6,6 +6,10 @@ import org.jetbrains.annotations.NotNull;
 
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class App extends AnAction {
     @Override
@@ -16,8 +20,28 @@ public class App extends AnAction {
             public void run() {
                 TimerView timerView = new TimerView();
                 timerView.setVisible(true);
+                System.out.println(getActualLines("/home/stefan/Stefan Projects/productivity-tracker/Productivity-Tracker"));
             }
         });
+    }
+
+    public static int getActualLines(String path){
+        File projectDirectory = new File(path);
+        int lineCount = 0;
+        for (File file : projectDirectory.listFiles()) {
+            if (file.isFile() && (file.getName().endsWith(".java") )) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    while (reader.readLine() != null) {
+                        lineCount++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if(file.isDirectory()){
+                lineCount += getActualLines(file.getPath());
+            }
+        }
+        return lineCount;
     }
 
 }
